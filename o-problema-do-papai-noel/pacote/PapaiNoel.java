85f468c8850f808ea3e29690fc529e0333bdf58c
jomarcardoso <antonionetocardoso@gmail.com>
2021-09-30 22:20:43 -0300

    fix: 🐛 rena

--- o-problema-do-papai-noel/pacote/PapaiNoel.java
+++ o-problema-do-papai-noel/pacote/PapaiNoel.java
@@ -7,75 +7,65 @@ public class PapaiNoel extends Thread {
   private ArrayList<Elfo> filaElfos = new ArrayList<Elfo>();
   private EstadoPapaiNoel estado = EstadoPapaiNoel.DORMINDO;
 
-  private void entregar() throws InterruptedException {
+  synchronized private void entregar() throws InterruptedException {
     if(this.filaRenas.size() >= 8) {
       this.filaRenas.clear();
+
       System.out.println("partiu entregar presentes");
+
       this.estado = EstadoPapaiNoel.DISTRIBUINDO_PRESENTES;
+      notifyAll();
       Thread.sleep((int)(8000));
       this.estado = EstadoPapaiNoel.ACORDADO;
+      notifyAll();
+
       System.out.println("acabou o natal nao precisa mais fazer entregas");
     }
   }
 
   synchronized private void discutir() throws InterruptedException {
     if(this.filaElfos.size() >= 3) {
-      // synchronized(this.filaElfos) {
-        this.filaElfos.remove(0);
-        this.filaElfos.remove(0);
-        this.filaElfos.remove(0);
-        // this.filaElfos.notifyAll();
-      // }
+      this.filaElfos.remove(0);
+      this.filaElfos.remove(0);
+      this.filaElfos.remove(0);
 
       System.out.println("partiu discutir com os elfos");
 
-      // synchronized(this.estado) {
-        this.estado = EstadoPapaiNoel.DISCUTINDO_PROJETOS;
-        notifyAll();
-        // this.estado.notifyAll();
-        Thread.sleep((int)(4000));
-        System.out.println("acabou de ajudar os elfos");
-        this.estado = EstadoPapaiNoel.ACORDADO;
-        // this.estado.notifyAll();
-      // }
+      this.estado = EstadoPapaiNoel.DISCUTINDO_PROJETOS;
+      notifyAll();
+      Thread.sleep((int)(4000));
+      System.out.println("acabou de ajudar os elfos");
+      this.estado = EstadoPapaiNoel.ACORDADO;
 
-        notifyAll();
+      notifyAll();
     }
   }
 
   synchronized public void adicionarElfoAFila(Elfo elfo) throws InterruptedException {
-    // synchronized(this.filaElfos) {
-      this.filaElfos.add(elfo);
+    this.filaElfos.add(elfo);
 
-      while(this.filaElfos.contains(elfo)) {
-        // Thread.sleep((int)(1));
-        System.out.println(elfo.name + " na fila");
-        wait();
-      }
+    while(this.filaElfos.contains(elfo)) {
+      System.out.println(elfo.name + " na fila");
+      wait();
+    }
 
-      while(this.estado == EstadoPapaiNoel.DISCUTINDO_PROJETOS) {
-        // Thread.sleep((int)(1));
-        System.out.println(elfo.name + " sendo atendido");
-        wait();
-      }
-      // }
+    while(this.estado == EstadoPapaiNoel.DISCUTINDO_PROJETOS) {
+      System.out.println(elfo.name + " sendo atendido");
+      wait();
+    }
 
-      System.out.println(elfo.name + " foi atendido");
+    System.out.println(elfo.name + " foi atendido");
   }
 
-  public void adicionarRenaAFila(Rena rena) throws InterruptedException {
+  synchronized public void adicionarRenaAFila(Rena rena) throws InterruptedException {
     this.filaRenas.add(rena);
 
-    synchronized(this.filaRenas) {
-      while(estado != EstadoPapaiNoel.DISTRIBUINDO_PRESENTES) {
-        // Thread.sleep((int)(1));
-        rena.wait();
-      }
+    while(estado != EstadoPapaiNoel.DISTRIBUINDO_PRESENTES) {
+      wait();
+    }
 
-      while(estado == EstadoPapaiNoel.DISTRIBUINDO_PRESENTES) {
-        // Thread.sleep((int)(1));
-        rena.wait();
-      }
+    while(estado == EstadoPapaiNoel.DISTRIBUINDO_PRESENTES) {
+      wait();
     }
   }
 
